72175883c5bc8240724967031a7e052a3b6b9b82
AquIce <timeo.perrinjaquet@gmail.com>
2023-12-09 17:21:41 +0100

    Added dynamic site

--- site/src/js/main.js
+++ site/src/js/main.js
@@ -17,91 +17,238 @@ const Event =  {
 
 const events = [
     {
-        'name': 'Evenement',
+        'name': 'Guerre',
         'date': 1998,
-        'type': 0,
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'refs': {},
+        'type': Event.Types.conflict,
+        'text': 'Lorem ipsum dolor sit amet, [consectetur adipiscing elit], sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. [img_0]',
+        'refs': {
+			'img_0': 'arrow.png',
+			'consectetur adipiscing elit': 'https://www.google.com',
+		},
         'tags': [],
     },
     {
-        'name': 'Evenement',
+        'name': 'Coupe du Monde',
         'date': 1995,
-        'type': 2,
+        'type': Event.Types.sport,
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'refs': {},
         'tags': [],
     },
     {
-        'name': 'Evenement',
+        'name': 'Élections',
         'date': 1991,
-        'type': 3,
+        'type': Event.Types.politics,
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'refs': {},
         'tags': [],
     },
     {
-        'name': 'Evenement',
+        'name': 'Villa turque',
         'date': 1988,
-        'type': 4,
-        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-        'refs': {},
+        'type': Event.Types.artch,
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod [img_villa] tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'refs': {
+			'img_villa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a6/CF05.jpg/1200px-CF05.jpg'
+		},
         'tags': [],
     },
     {
-        'name': 'Evenement',
+        'name': 'Crash boursier',
         'date': 1994,
-        'type': 1,
+        'type': Event.Types.economy,
         'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         'refs': {},
         'tags': [],
     },
 ]
 
+const prepareEvent = event => {
+	if(event.refs) {
+		for(const ref of Object.keys(event.refs)) {
+			if(ref.startsWith('img_')) {
+				event.text = event.text.replace(`[${ref}]`, `<style>img { width: 100% }</style><img src="${!event.refs[ref].startsWith('http') ? './src/img/' : ''}${event.refs[ref]}" alt="${event.refs[ref].split('.')[0]}">`)
+			} else {
+				event.text = event.text.replace(`[${ref}]`, `<a href="${event.refs[ref]}">${ref}</a>`)
+			}
+		}
+	}
+}
+
 const h_unit_offset = 42
 const event_width = 300
 
 const insideDiv = (div, event, is_left) => {
+	
+	/**
+	 * Div containing the arrow
+	 */
 	const arrow_div = document.createElement('div')
-	arrow_div.style.height = '100%'
 	arrow_div.innerHTML = `<img src="./src/img/arrow.png" alt="arrow" class="${is_left ? 'right-arrow' : 'left-arrow'}">`
+	
+	/**
+	 * Div containing the name
+	 */
 	const name_div = document.createElement('div')
 	name_div.innerHTML = event.name
+	
+	/**
+	 * Div containing the date
+	 */
 	const date_div = document.createElement('div')
 	date_div.innerHTML = event.date
+
+	// Append all children
 	div.appendChild(arrow_div)
 	div.appendChild(name_div)
 	div.appendChild(date_div)
+
+	// Setup for the inside div
 	div.style.display = 'flex'
 	div.style.flexDirection = 'row'
 	div.style.justifyContent = 'space-between'
 	div.style.alignItems = 'center'
-	div.style.padding = '0 15px 0 15px'
+}
+
+const destroyContent = full_div => {
+	full_div.style.width = `${event_width}px`
+	full_div.style.height = `${h_unit_offset}px`
+	full_div.removeChild(full_div.lastChild)
+}
+
+const findName = name => {
+	for(const event of events) {
+		if(event.name === name) {
+			return events.indexOf(event)
+		}
+	}
+}
+
+const extendInvert = (event, full_div) => {
+	if(event.extended) {
+		destroyContent(full_div)
+	} else {
+		const full_divs = document.querySelectorAll('.full-div')
+		for(const ev of full_divs) {
+			const index = findName(ev.firstChild.childNodes[1].innerHTML)
+			if(events[index].extended) {
+				events[index].extended = false
+				destroyContent(ev)
+			}
+		}
+
+		
+		full_div.style.width = `${event_width + 100}px`
+		full_div.style.height = 'auto'
+
+		/**
+		 * Div containing the event's text
+		 */
+		const overdiv = document.createElement('div')
+		full_div.style.marginLeft = '0'
+		const align_div = document.createElement('div')
+		align_div.style.backgroundColor = '#f5f5f5'
+		const text_div = document.createElement('div')
+		text_div.classList.add('text-div')
+		text_div.innerHTML = `${event.text}`
+		text_div.style.margin = '15px'
+		text_div.style.color = 'white'
+		text_div.style.fontSize = '16px'
+		text_div.style.textAlign = 'justify'
+
+		overdiv.style.display = 'grid'
+
+		if(Array.from(document.querySelector('#left-events').children).indexOf(full_div) !== -1) {
+			overdiv.appendChild(text_div)
+			overdiv.appendChild(align_div)
+			overdiv.style.gridTemplateColumns = '1fr 50px'
+		} else {
+			overdiv.appendChild(align_div)
+			overdiv.appendChild(text_div)
+			overdiv.style.gridTemplateColumns = '50px 1fr'
+		}
+
+
+		full_div.appendChild(overdiv)
+	}
+	event.extended = !event.extended
+	return event
 }
 
 const makeEvent = (event, is_left) => {
-    const h_offset = (2000 - event.date) * h_unit_offset
+
+	/**
+	 * Offset from the top of the page (depends on the date)
+	 */
+	const h_offset = (2000 - event.date) * h_unit_offset + 20
+	
+	/**
+	 * Color of the event (depends on the type)
+	 */
     const color = Event.Colors(event.type)
+	
+	/**
+	 * Div containing the event
+	 */
+	const full_div = document.createElement('div')
+	full_div.classList.add('full-div')
+	full_div.style.backgroundColor = color
+	full_div.style.position = 'absolute'
+	full_div.style.top = `${h_offset}px`
+	full_div.style.height = `${h_unit_offset}px`
+	full_div.style.width = `${event_width}px`
+	full_div.style.display = 'flex'
+	full_div.style.flexDirection = 'column'
+	full_div.style.justifyContent = 'start'
+	
+	/**
+	 * Div containing the event's content
+	 */
 	const event_div = document.createElement('div')
 	event_div.classList.add('event')
-	event_div.style.marginTop = `${h_offset}px`
-	event_div.style.backgroundColor = color
 	event_div.style.height = `${h_unit_offset}px`
-	event_div.style.width = `${event_width}px`
+	event_div.style.width = 'calc(100% - 30px)'
+	event_div.style.marginLeft = '15px'
+
+	// Inside div setup
 	insideDiv(event_div, event, is_left)
+
+	// Setup for right or left div
 	if(is_left) {
-		event_div.style.marginRight = `${h_unit_offset}px`
-		event_div.style.marginLeft = `calc(100% - ${event_width}px - ${h_unit_offset + 15 * 2}px)` 
-		event_div.style.flexDirection = 'row-reverse'
+		full_div.style.right = `${h_unit_offset}px`
+		event_div.firstChild.firstChild.style.rotate = '180deg'
 	} else {
-		event_div.style.marginLeft  = `${h_unit_offset}px`
+		event_div.style.flexDirection = 'row-reverse'
+		full_div.style.left  = `${h_unit_offset}px`
 	}
-	document.querySelector(`#${is_left ? 'left' : 'right'}-events`).appendChild(event_div)
+
+	// Event listener for the full div
+	event_div.addEventListener('click', () => { event = extendInvert(event, full_div) })
+
+	full_div.appendChild(event_div)
+	
+	document.querySelector(`#${is_left ? 'left' : 'right'}-events`).appendChild(full_div)
 }
 
+const compareByDate = (a, b) => b.date - a.date
+
 window.onload = () => {
-    for(let i = 0; i < events.length; i++) {
+	// Set height of the main container (for 100 years)
+	document.querySelector('#main-container').style.height = 100 * h_unit_offset  + 75 + 'px'
+
+	// Sort events by date inversed but then display them in the right order
+	events.sort(compareByDate)
+    for(let i = events.length - 1; i >= 0; i--) {
         const event = events[i]
+		prepareEvent(event)
+		event.extended = false
         makeEvent(event, i % 2)
     }
+
+	// Set position of the milestones
+	for(const milestone of document.querySelectorAll('#timeline > div')) {
+		milestone.style.position = 'absolute'
+		milestone.style.top = `${(2000 - parseInt(milestone.innerHTML)) * h_unit_offset + 25}px`
+		milestone.style.left = `${(100 - milestone.offsetWidth) / 2}px`
+	}
 }
