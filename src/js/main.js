69d28f946b63f20d877d0b0dfdac91ccd028e928
Timeo Perrinjaquet <99663083+AquIce@users.noreply.github.com>
2024-02-23 12:03:49 +0100

    Add files via upload

--- src/js/main.js
+++ src/js/main.js
@@ -36,6 +36,7 @@ const E = [
         'text': 'La Maison Blanche, aussi appelée villa Jeanneret-Perret, est conçue par l’architecte Le Corbusier et construite en 1912. Se situant sur le haut de la ville de la Chaux-de-Fonds avec une architecture unique, cette maison est le résultat de nombreuses inspirations dues à ses voyages.[img_0]Dans un premier temps destinée aux parents du Corbusier, elle sera vendue peu après, en 1919, puis plusieurs propriétaires s’y succéderont. C’est en 2000 que la maison sera rachetée par une association et ouvrira ses portes au public en 2005',
         'refs': {
             'img_0':'https://maisonblanche.ch/wp-content/uploads/2020/03/amb_hiver.jpg'
+			
 		},
         'tags': [
 			'architecture',
@@ -44,11 +45,12 @@ const E = [
 		],
     },
     {
-        'name': 'Hockey club la Chaux-de-Fonds',
+        'name': 'Création du HCC',
         'date': 1919,
         'type': Event.Types.sport,
         'text': 'Le 6 février 1919 la section hockey apparaît pour la première fois dans la ville. À cette époque la mauvaise qualité de glace, pousse le club à déménager quand il le faut sur le Lac des Taillères. Il faudra attendre 1951 pour que le club soit pour la première fois champion de Swiss League (2ème division suisse). Après 4 titres en 2ème division, le club fêtera son premier titre de National League (1er division suisse) fin de saison 1967/1968. Les 5 saisons suivantes seront un énorme succès avec 5 nouveaux titres de champion Suisse. Aujourd’hui le club Chaux-de-Fonnier évolue en 2ème division suisse et est soutenu par toute une région.<br /> La patinoire artificielle des Mélèzes créée en 1953, sera couverte en 1968.',
         'refs': {
+            
 			
 		},
         'tags': [
@@ -57,6 +59,7 @@ const E = [
 			'sport',
 			'loisirs'
 		],
+
     },
     {
         'name': 'Première Braderie',
@@ -65,12 +68,14 @@ const E = [
         'text': 'La Braderie a été créée en 1932 pendant une grande crise économique qui touchait la Chaux-de fonds. La ville, qui comptait beaucoup de chômeurs et avec de grandes quantités de marchandises invendues, a décidé de créer une grande foire qui permettrait aux commerçants de vider leurs stocks. Aujourd’hui la Braderie est une des plus grandes fête Chaux-de-Fonnière et réunit des habitants de toute la région pour ses animations et ses cortèges.[img_0]',
         'refs': {
            'img_0':'https://le-o.ch/wp-content/uploads/2022/09/photo_carrousel_c_-Museedhistoire.jpg' 
+			
 		},
         'tags': [
 			'économie',
 			'loisirs',
 			'la Chaux-de-Fonds'
 		],
+        
     },
     {
         'name': 'Première femme au Grand Conseil',
@@ -79,26 +84,93 @@ const E = [
         'text': 'En 1959, les Neuchâtelois acceptent de donner le droit de vote aux femmes et ainsi ouvrent l’opportunité aux électrices neuchâteloises de se présenter. Une année après, la socialiste Raymond Schweizer est la première femme élue en Suisse, à la faveur d’une élection partielle au Grand Conseil, elle y restera jusqu’en 1969. Première suissesse élue dans un parlement cantonal qui a lutté pour l’égalité des hommes et des femmes.',
         'refs': {
            'img_0':'https://imagesdupatrimoine.ch/wp-content/uploads/2022/02/La-premiere-suissesse-a-sieger-dans-un-legislatif-cantonal.jpg' 
+			
 		},
         'tags': [
 			'politique',
 			'droits des femmes',
 		],
+        
     },
     {
         'name': 'Tremplin de la Combe-Girard',
         'date': 1932,
         'type': Event.Types.sport,
-        'text': 'La construction de ce tremplin a fait naître un nouveau sport dans le Canton, avec un engouement gigantesque. La population se ruait en bas de la piste afin de contempler les sauts des skieurs. Le record du Tremplin est de 86,5 mètres détenus par un Suisse. Le Tremplin de la Combe-Girard est parmi les plus légendaires de Suisse. En effet, durant 60 ans, il accueillera de nombreuses compétitions dont une très célèbre, la Tournée Suisse des quatre tremplins. Puis dans les années 90 le tremplin sera laissé à l’abandon et se détériorera avec le temps.',
+        'text': 'La construction de ce tremplin a fait naître un nouveau sport dans le Canton, avec un engouement gigantesque. La population se ruait en bas de la piste afin de contempler les sauts des skieurs. [img_0]Le record du Tremplin est de 86,5 mètres détenus par un Suisse. Le Tremplin de la Combe-Girard est parmi les plus légendaires de Suisse. En effet, durant 60 ans, il accueillera de nombreuses compétitions dont une très célèbre, la Tournée Suisse des quatre tremplins. Puis dans les années 90 le tremplin sera laissé à l’abandon et se détériorera avec le temps.',
         'refs': {
-			
+           'img_0': 'tremplin.jpg'
 		},
         'tags': [
 			'le Locle',
 			'sport',
 			'loisirs'
 		],
-    }
+    },
+    {
+        'name': 'Nouvelle gare à la Chaux-de-Fonds',
+        'date': 1903,
+        'type': Event.Types.artch,
+        'text': 'Au milieu du 19ème siècle, la première gare de la ville fut construite pour des raisons commerciales et afin de montrer que la ville est en plein développement industriel. Cette gare sera réaménagée plusieurs fois jusqu’en 1901. Cependant l’évolution industrielle trop importante de la ville fit que la gare n’était plus suffisante. C’est ainsi qu’en 1903 commence les travaux de la nouvelle gare. Inauguré en 1904, la nouvelle gare de la Chaux-de-Fonds ne changera presque plus jusqu’à nos jours.[img_0]',
+        'refs': {
+           'img_0':'https://imagesdupatrimoine.ch/wp-content/uploads/2022/02/La-nouvelle-gare-de-La-Chaux-de-Fonds.jpg'
+		},
+        'tags': [
+			'la Chaux-de-Fonds',
+		],
+    },
+	{
+        'name': 'Grève générale',
+        'date': 1918,
+        'type': Event.Types.politics,
+        'text': 'La grève générale de 1918 fut un mouvement social majeur qui a impliqué des travailleurs de divers secteurs à Neuchâtel et à travers tout le pays. Les ouvriers, les femmes et même les enfants ont participé à cette protestation massive contre les conditions de travail difficiles, les bas salaires et la pénurie alimentaire due à la Première Guerre mondiale. Les principales revendications portaient sur la réduction du temps de travail, des salaires décents, une meilleure distribution des denrées alimentaires et même le droit de vote pour les femmes. Cette grève a fortement perturbé l\'économie suisse et a poussé le gouvernement à prendre des mesures pour améliorer les conditions de vie des travailleurs.[img_0]',
+        'refs': {
+           'img_0':'https://www.psmne.ch/sites/psmne.ch/files/styles/content_full/public/images/grevegeneraleaulocle.jpg?itok=VQmFb1po'
+		},
+        'tags': [
+			'politique'
+		],
+    },
+	{
+        'name': 'Cyclone dévaste l’arc jurassien',
+        'date': 1926,
+        'type': Event.Types.politics,
+        'text': 'Il y a presque cent ans un phénomène météorologique avait, comme récemment, touché la région. En effet, c’est le samedi 12 juin 1926 aux alentours de 18h que surgit une tornade faisant de lourds dégâts sur son passage. On a recensé un mort, plusieurs blessés et plus de 200 bâtiments endommagés.[img_0]',
+        'refs': {
+           'img_0':'cyclone.jpg'
+		},
+        'tags': [
+			'catastrophe'
+		],
+    },
+	{
+        'name': 'Bombardement de la Chaux-de-Fonds',
+        'date': 1915,
+        'type': Event.Types.conflict,
+        'text': 'C\'est aux alentours de 15h le dimanche 17 octobre 1915 que les citoyens de la Chaux-de-Fonds entendent des détonations. La Chaux-de-Fonds venait d’être bombardée à cinq reprises. Heureusement, seulement quelques blessés sont à déplorer.[img_0] C\’est évènement marquera les esprits des Chaux-de-Fonniers. On retrouvait même des enfants qui vendait des fragments de bombe en tant que « souvenirs du bombardement de la Chaux-de-Fonds »',
+        'refs': {
+           'img_0':'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRlWlQpUjq-16lN9r-9h2EbLJiursbFBGmFovXnMX32dQ&s'
+		},
+        'tags': [
+			'la Chaux-de-Fonds',
+			'catastrophe'
+		],
+    },
+	
+	{
+        'name': 'Premier salon de l’horlogerie',
+        'date': 1933,
+        'type': Event.Types.economy,
+        'text': 'Après le succès de l\'Exposition d\'horlogerie ancienne et moderne, ses organisateurs ont décidé de lancer le Salon suisse de l’horlogerie. Sa première édition s’est déroulée entre le 25 aout et le 18 septembre 1933 et bien évidemment à la Chaux-de-Fonds.[img_0] Le Salon se voulait moderne, avec toutes les dernières création et concepts de l’horlogerie de l’époque. D’un autre côté, le Salon avait aussi une partie historique et exposera une série de pièces rares et historique qui ont fait maintes fois parlées d’elles.',
+        'refs': {
+           'img_0':'https://i0.wp.com/grail-watch.com/wp-content/uploads/2023/06/Premier-Salon-Suisse-de-lHorlogerie-Fiedler-scaled.jpg?resize=350%2C512&ssl=1'
+			
+		},
+        'tags': [
+			'la Chaux-de-Fonds',
+			'économie',
+			'horlogerie'
+		],
+    },
 ]
 
 let events = []
@@ -142,8 +214,9 @@ const prepareEvent = event => {
 			if(ref.startsWith('img_')) {
 				// Preload the image to prevent height calculation problems
 				const img = new Image()
+				const img_src_text = event.refs[ref].startsWith('http') ? `<a href=${event.refs[ref]}>${event.refs[ref]}</a>` : '<a href="https://mhcdf.ch/">Musée d\'histoire de la Chaux-de-Fonds</a>'
 				img.src = `${!event.refs[ref].startsWith('http') ? './src/img/' : ''}${event.refs[ref]}`
-				event.text = event.text.replace(`[${ref}]`, `<style>img { width: 100% }</style><img src="${img.src}" alt="${event.refs[ref].split('.')[0]}">`)
+				event.text = event.text.replace(`[${ref}]`, `<figure><img width="100%" src="${img.src}" alt="${event.refs[ref].split('.')[0]}"><figcaption>${img_src_text}</figcaption></figure>`)
 			} else {
 				event.text = event.text.replace(`[${ref}]`, `<a href="${event.refs[ref]}">${ref}</a>`)
 			}
@@ -158,7 +231,7 @@ const H_UNIT_OFFSET = 42
 /**
  * The width of an event div (not extended)
  */
-const EVENT_WIDTH = 350
+const EVENT_WIDTH = 400
 /**
  * The number of years to display
  */
@@ -367,7 +440,7 @@ const makeEvent = (event, is_left) => {
 	/**
 	 * Offset from the top of the page (depends on the date)
 	 */
-	const h_offset = (MAX_YEAR - event.date) * H_UNIT_OFFSET + 20
+	const h_offset = (MAX_YEAR - event.date) * (H_UNIT_OFFSET + 10) + 25
 	
 	event.color = Event.Colors(event.type)
 	event.text_color = Event.TextColors(event.type)
@@ -429,7 +502,6 @@ const refreshEvents = () => {
 	document.querySelector('#left-events').innerHTML = ''
 	document.querySelector('#right-events').innerHTML = ''
 	events = filterDivs(active_filters)
-	console.log(events)
 	events.sort(compareByDate)
 	for(let i = 0; i < events.length; i++) {
 		const event = events[i]
@@ -443,7 +515,7 @@ window.onload = () => {
 	document.querySelector('html').style.backgroundImage = `url('src/img/background${Math.floor(Math.random() * 5)}.jpg')`
 	// Set height of the main container
 	const main_container = document.querySelector('#main-container')
-	main_container.style.height = `${YEARS * H_UNIT_OFFSET + 90}px`
+	main_container.style.height = `${YEARS * (H_UNIT_OFFSET + 10) + 90}px`
 	main_container.style.gridTemplateColumns = `1fr ${TIMELINE_WIDTH}px 1fr`;
 	for(let i = MAX_YEAR; i >= MAX_YEAR - YEARS; i -= TIMELINE_GAP_YEARS) {
 		const div = document.createElement('div')
@@ -457,7 +529,7 @@ window.onload = () => {
 	// Set position of the milestones
 	for(const milestone of document.querySelectorAll('#timeline > div')) {
 		milestone.style.position = 'absolute'
-		milestone.style.top = `${(MAX_YEAR - parseInt(milestone.innerHTML)) * H_UNIT_OFFSET + 25}px`
+		milestone.style.top = `${(MAX_YEAR - parseInt(milestone.innerHTML)) * (H_UNIT_OFFSET + 10) + 25}px`
 		milestone.style.left = `${(TIMELINE_WIDTH - milestone.offsetWidth) / 2}px`
 	}
 
@@ -549,9 +621,10 @@ const refreshResults = () => {
 		highlightDiv(0)
 		return
 	}
-	for(const filter of filters.filter(el => !active_filters.includes(el))) {
+	for(const filter of filters.filter(el => !active_filters.includes(el)).sort((a, b) => a[0] > b[0])) {
 		if(filter.startsWith(input.value)) {
 			const filterdiv = document.createElement('div')
+			filterdiv.style.color = 'white'
 			filterdiv.innerHTML = filter
 			results.appendChild(filterdiv)
 		}
@@ -577,6 +650,7 @@ const keyListener = e => {
 			input.focus()
 			break
 		case 'Tab':
+		case 'Enter':
 			e.preventDefault()
 			addEvent()
 			break
